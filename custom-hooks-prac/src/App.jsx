9f66c5a12d01de4d2ec359d3d230826c63bcf241
Vikrant <86877242+Vikrant-1@users.noreply.github.com>
2024-08-17 05:20:45 +0000

    how state work in clas vs functional component

--- custom-hooks-prac/src/App.jsx
+++ custom-hooks-prac/src/App.jsx
@@ -1,22 +1,44 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-// function componentss
-function MyCompnent() {
-  const [count, setCount] = useState(0);
+// function components state based
+// function MyCompnent() {
+//   const [count, setCount] = useState(0);
 
-  return (
-    <div>
-      <p>{count}</p>
-      <button onClick={() => setCount(count + 1)}>Increament</button>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <p>{count}</p>
+//       <button onClick={() => setCount(count + 1)}>Increament</button>
+//     </div>
+//   );
+// }
+
+// class component state based
+// class MyCompnent2 extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { count: 0 };
+//   }
+
+//   incrementCount = () => {
+//     this.setState({ count: this.state.count + 1 });
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <p>{this.state.count}</p>
+//         <button onClick={() => this.incrementCount()}>Increament</button>
+//       </div>
+//     );
+//   }
+// }
 
 function App() {
   return (
     <>
       <MyCompnent />
+      <MyCompnent2 />
     </>
   );
 }
